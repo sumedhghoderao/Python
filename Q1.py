0f2c61399c81f6e7eaad7f099580dabdb8936c69
sumedhghoderao <sumedhghoderao6825@gmail.com>
2023-10-27 13:34:02 +0530

    doit

---
+++ Q1.py
@@ -0,0 +1,7 @@
+num1,num2 = int(input("Enter First number")),int(input("Enter Second Number"))
+add=num1+num2
+square=num1*num1
+power=num1**num2
+print("Addition is ",add)
+print("square is ",square)
+print("power is ",power)
